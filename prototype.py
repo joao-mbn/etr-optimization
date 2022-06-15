976ee10b7e95f289c83c42a8eb5a30cfb670a6ef
João <joaombn97@gmail.com>
2022-06-14 23:31:59 -0300

    clean prototype

--- prototype.py
+++ prototype.py
@@ -1,6 +1,7 @@
 
-from _generate_data import calculate
 from _generate_charts import generate_charts
+from _generate_data import calculate
+from projects.nd_sm_cut import projects
 
 calculate(projects)
 generate_charts(True)
