c46881f9446b7ec8f1691d09943556dee54ce1e9
João <joaombn97@gmail.com>
2022-06-16 00:08:47 -0300

    Minor configure changes to surface plot. Hiding scatter plot.

--- visualization/_cost_relationship_surface.py
+++ visualization/_cost_relationship_surface.py
@@ -1,19 +1,22 @@
-from matplotlib.cm import ScalarMappable
-import matplotlib.pyplot as plt
 import matplotlib.colors as mpl_colors
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from visualization._regression import get_regression, predict
 from global_constants import CHARTS_RESULTS_FOLDER_PATH
+from matplotlib.cm import ScalarMappable
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from sklearn.linear_model import LinearRegression
 from templates._types import Number
+
+from visualization._regression import get_regression, predict
+
 
 def cost_relationship_surface(df_slice: pd.DataFrame, save_fig: bool = False):
 
     regression = get_regression(df_slice)
     extractant_name, extractant_concentration = df_slice[['extractant', 'extractant concentration']].mode().values[0]
 
-    fig = plt.figure()
+    fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection='3d')
     mappable = plt.cm.ScalarMappable(cmap='Spectral')
 
@@ -25,16 +28,14 @@ def cost_relationship_surface(df_slice: pd.DataFrame, save_fig: bool = False):
     ax.set_zlabel('Custo (USD)')
     ax.view_init(elev=35, azim=45)
     plt.legend(loc='lower center', bbox_to_anchor=(0.5, 1))
-    plt.colorbar(mappable)
+    plt.colorbar(mappable, shrink = 0.5)
     plt.tight_layout()
 
     plt.show() if not save_fig else plt.savefig(f'{CHARTS_RESULTS_FOLDER_PATH}Superfície {extractant_name} {round(extractant_concentration * 100)}%.png',
                                                 bbox_inches='tight')
 
 def create_surface(ax, regression: LinearRegression, df: pd.DataFrame, mappable: ScalarMappable):
-
-    # Creates data for surface, predicting results from the model
-    n_cells, ao_ratio = np.meshgrid(np.sort(df['n cells'].unique()), np.sort(df['ao ratio'].unique()))
+    n_cells, ao_ratio = np.meshgrid(np.linspace(df['n cells'].min(), df['n cells'].max(), 100), np.linspace(df['ao ratio'].min(), df['ao ratio'].max(), 100))
     pHi, extractant, extractant_concentration = df[['pHi', 'extractant', 'extractant concentration']].mode().values[0]
 
     features = pd.DataFrame({'n cells': n_cells.ravel(), 'ao ratio': ao_ratio.ravel()})
@@ -44,13 +45,13 @@ def create_surface(ax, regression: LinearRegression, df: pd.DataFrame, mappable:
 
     mappable.set_array(out)
     plot = ax.plot_surface(n_cells, ao_ratio, out,
-                           rstride = 1, cstride = 1, alpha = 0.4, cmap = mappable.cmap, norm = mappable.norm,
-                           label=f'pH = {round(pHi, 1)}, {extractant} {round(extractant_concentration * 100)}%')
+                           rstride = 1, cstride = 1, alpha = 0.8, cmap = mappable.cmap, norm = mappable.norm,
+                           label=f'{extractant} {round(extractant_concentration * 100)}%')
     plot._facecolors2d = plot._facecolor3d
     plot._edgecolors2d = plot._edgecolor3d
 
 def create_scatter_plot(df: pd.DataFrame, ax):
-    transparencies = create_property_scale(df, 'pHi', (0.3, 1))
+    transparencies = create_property_scale(df, 'pHi', (0, 0))
     marker_sizes = create_property_scale(df, 'pHi', (0.5, 1), False)
     colors = create_property_scale(df, 'extractant').apply(lambda hue: mpl_colors.hsv_to_rgb((hue, 0, 0)))
 
