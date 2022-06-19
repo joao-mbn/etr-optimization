cf96e9fca40009fd80edda04619b98d8eaecfd02
João <joaombn97@gmail.com>
2022-06-19 19:25:10 -0300

    Uncommented code.

--- projects/pr_nd_sm_cut.py
+++ projects/pr_nd_sm_cut.py
@@ -8,8 +8,8 @@ from templates._models import Project
 
 cut = 'Nd/Sm'
 distribution_ratio_model = logD_x_pH
-max_cells_interval = (2, 10)
-pHi_interval = (1, 2.5)
+max_cells_interval = (2, 15)
+pHi_interval = (1, 2)
 ao_ratio_interval = (0.5, 2)
 required_raffinate_purity = 0.995
 minimal_recovery = 0.15
