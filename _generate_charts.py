6867d7abe50c8f93e7fb449c7a6f01177d3f62e0
João <joaombn97@gmail.com>
2022-06-26 16:02:32 -0300

    Updated new charts

--- _generate_charts.py
+++ _generate_charts.py
@@ -10,32 +10,35 @@ from visualization._predicted_costs_tri_surface import predicted_costs_tri_surfa
 from visualization._recovery_cost_relationship_chart import recovery_cost_relationship_chart
 
 
-def generate_charts(save_fig: bool = False):
+def generate_charts(save_fig: bool = False) -> None:
 
-    cost_df = pd.read_excel(f'{ROOT_PATH}{APPROVEDS_ONLY_EXCEL}', COST_TAB)
-    standardized_cost_df = pd.read_excel(f'{ROOT_PATH}{APPROVEDS_ONLY_EXCEL}', STANDARDIZED_COST_TAB)
     detailed_cost_df = pd.read_excel(f'{ROOT_PATH}{APPROVEDS_ONLY_EXCEL}', DETAILED_COST_TAB)
     complete_results_df = pd.read_excel(f'{ROOT_PATH}{APPROVEDS_ONLY_EXCEL}', COMPLETE_RESULTS_TAB)
+
+    all_conditions_cost_df = pd.read_excel(f'{ROOT_PATH}{ALL_CONDITIONS_EXCEL}', COST_TAB)
+    all_conditions_standardized_cost_df = pd.read_excel(f'{ROOT_PATH}{ALL_CONDITIONS_EXCEL}', STANDARDIZED_COST_TAB)
     all_conditions_complete_results_df = pd.read_excel(f'{ROOT_PATH}{ALL_CONDITIONS_EXCEL}', COMPLETE_RESULTS_TAB)
 
-    pareto_chart(get_regression_comprehensive_coefficients_table(standardized_cost_df), save_fig)
     detailed_cost_chart(detailed_cost_df, save_fig)
     recovery_cost_relationship_chart(complete_results_df, save_fig)
-    sliced_charts(cost_df, standardized_cost_df, all_conditions_complete_results_df, save_fig)
+
+    pareto_chart(get_regression_comprehensive_coefficients_table(all_conditions_standardized_cost_df), save_fig)
+    sliced_charts(all_conditions_cost_df, all_conditions_standardized_cost_df, all_conditions_complete_results_df, save_fig)
 
 
-def sliced_charts(cost_df, standardized_cost_df, all_conditions_complete_results_df, save_fig):
-    for extractant_name in all_conditions_complete_results_df['extractant'].unique().tolist():
-        for extractant_concentration in all_conditions_complete_results_df['extractant concentration'].unique().tolist():
+def sliced_charts(cost_df, standardized_cost_df, complete_results_df, save_fig):
+
+    for extractant_name in complete_results_df['extractant'].unique().tolist():
+        for extractant_concentration in complete_results_df['extractant concentration'].unique().tolist():
 
             cost_df_slice = cost_df.query(f'extractant == "{extractant_name}" and `extractant concentration` == {extractant_concentration}')
             if len(cost_df_slice) > 0:
                 cost_relationship_surface(cost_df_slice, save_fig)
 
-            all_conditions_complete_results_df_slice = all_conditions_complete_results_df.query(f'extractant == "{extractant_name}" and `extractant concentration` == {extractant_concentration}')
-            if len(all_conditions_complete_results_df_slice) > 0:
-                predicted_costs_tri_surface(all_conditions_complete_results_df_slice, save_fig)
-
-            standardized_cost_df_slice = standardized_cost_df.query(f'extractant == "{extractant_name}" and `extractant concentration` == {extractant_concentration}')
+            standardized_cost_df_slice = standardized_cost_df[standardized_cost_df.index.isin(cost_df_slice.index)]
             if len(standardized_cost_df_slice) > 0:
                 pareto_chart(get_regression_comprehensive_coefficients_table(standardized_cost_df_slice), save_fig, extractant_name, extractant_concentration)
+
+            complete_results_df_slice = complete_results_df.query(f'extractant == "{extractant_name}" and `extractant concentration` == {extractant_concentration}')
+            if len(complete_results_df_slice) > 0:
+                predicted_costs_tri_surface(complete_results_df_slice, save_fig)
--- prototype.py
+++ prototype.py
@@ -4,4 +4,4 @@ from projects.nd_sm_cut import projects
 from _unautomated_methods import call_wrappers
 
 
-generate_charts(save_fig=False)
+generate_charts(True)
